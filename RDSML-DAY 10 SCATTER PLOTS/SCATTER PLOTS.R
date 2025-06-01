4a0eb62ff5a875cb51a1bc312c8596609efe41a4
TanwiDey <tanu.syl@gmail.com>
2025-06-01 08:50:43 +0600

    scatter plot2

--- RDSML-DAY 10 SCATTER PLOTS/SCATTER PLOTS.R
+++ RDSML-DAY 10 SCATTER PLOTS/SCATTER PLOTS.R
@@ -3,12 +3,17 @@ library("ggplot2")
 dataset = mtcars
 view(dataset)
 #gererating basic scatter plot, x,y axis shoed by aesthetic function 
-ggplot(dataset, aes(x = wt, y= mpg))+ 
-geom_point()
+
+ggplot(dataset, aes(x = wt, y= mpg))+ geom_point()
+
 #changing the point size and shape
+
 ggplot(dataset, aes(x = wt, y= mpg))+ 
-  geom_point(size = 2, shape = 15
+  geom_point(size = 2, shape = 15)
              
 #adding regression line
+
+ggplot(dataset, aes(x = wt, y= mpg))+ geom_point(size = 2, shape = 15) + geom_smooth()
+
 ggplot(dataset, aes(x = wt, y= mpg))+ 
-  geom_point(size = 2, shape = 15) + geom_smooth()
+  geom_point(size = 2, shape = 15, color = 'blue')+ geom_smooth(method = 1m, linetype = "dashed", color = "darkred",  fill = "blue")
